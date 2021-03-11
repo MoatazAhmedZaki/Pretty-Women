3304b9a7ba44329afc7a05493ce7619def64b190
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-11 18:11:12 +0200

    Updates

---
+++ precache-manifest.7d9800344a4195c18877cb74b41a8d85.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Pretty-Women/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "1e526beadab113fda992",
+    "url": "/Pretty-Women/static/css/main.d3b73883.chunk.css"
+  },
+  {
+    "revision": "f7a1fb1e731fdbaf2aac",
+    "url": "/Pretty-Women/static/js/runtime~main.f082f220.js"
+  },
+  {
+    "revision": "093466c99afdd5e38cfe3062dbcbba6b",
+    "url": "/Pretty-Women/static/media/Lato-Heavy.093466c9.ttf"
+  },
+  {
+    "revision": "d8edb309295476198b64",
+    "url": "/Pretty-Women/static/js/2.e6a54ee7.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Pretty-Women/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Pretty-Women/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Pretty-Women/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "1e526beadab113fda992",
+    "url": "/Pretty-Women/static/js/main.c8c0c175.chunk.js"
+  },
+  {
+    "revision": "56df7768ccfd838b9e1a9fdf74f9997b",
+    "url": "/Pretty-Women/static/media/logo.56df7768.png"
+  },
+  {
+    "revision": "50cc86cc8a3f10bb207fb696945988e3",
+    "url": "/Pretty-Women/static/media/pallete.50cc86cc.png"
+  },
+  {
+    "revision": "80e33be0923c8e2d03ca3d9b07aa1f59",
+    "url": "/Pretty-Women/static/media/MADE Canvas Black PERSONAL USE.80e33be0.otf"
+  },
+  {
+    "revision": "863b7dcd5ec2c3923122af25ce0f7e4c",
+    "url": "/Pretty-Women/static/media/Lato-Medium.863b7dcd.ttf"
+  },
+  {
+    "revision": "90e1d3559ac52f7f0f77a86e1bfd632d",
+    "url": "/Pretty-Women/static/media/Lato-Light.90e1d355.ttf"
+  },
+  {
+    "revision": "3b9b99039cc0a98dd50c3cbfac57ccb2",
+    "url": "/Pretty-Women/static/media/Lato-Regular.3b9b9903.ttf"
+  },
+  {
+    "revision": "d8edb309295476198b64",
+    "url": "/Pretty-Women/static/css/2.763719e6.chunk.css"
+  },
+  {
+    "revision": "7bbdfce9de09c408db9face34df8a517",
+    "url": "/Pretty-Women/index.html"
+  }
+];
